b4a922f33dded30273e7a293f52e2ffb0bec44bd
Mortisanti <moltigamer@gmail.com>
2021-08-09 02:33:22 -0400

    Removed old slash commands

--- main.py
+++ main.py
@@ -18,56 +18,6 @@ async def on_ready():
 
 
 # Databank List
-# @slash.slash(name="databanklist", description="Print out a numbered list of each databank entry and the level it corresponds to.")
-# async def databank_list(ctx):
-#     trimmed_lore_list = lore.get_lore_list()
-#     footer_icon_url = 'https://i.imgur.com/VpMy8Yr.gif'
-#     databank_embed = discord.Embed(
-#         type='rich',
-#         title="Databank List",
-#         description=trimmed_lore_list,
-#         color=0x00ffd9
-#         )
-#     # databank_embed.set_image(url=img_url)
-#     databank_embed.set_footer(text=f"Databank List", icon_url=footer_icon_url)
-#     await ctx.send(embed=databank_embed)
-
-
-# Databank Random
-# @slash.slash(name="databankrandom", description="Retrieve a random entry from the databank.")
-# async def databank_random(ctx):
-#     level, short_desc, img_url, long_desc = lore.get_lore()
-#     footer_icon_url = 'https://i.imgur.com/VpMy8Yr.gif'
-#     databank_embed = discord.Embed(
-#         type='rich',
-#         title=short_desc,
-#         description=long_desc,
-#         color=0x00ffd9,
-#         url=img_url
-#         )
-#     databank_embed.set_image(url=img_url)
-#     databank_embed.set_footer(text=f"From the Level: {level}", icon_url=footer_icon_url)
-#     await ctx.send(embed=databank_embed)
-
-
-# Databank Entry
-# @slash.slash(name="databankentry", description="Enter a number between 1-90 to retrieve a specific entry from the databank.")
-# async def databank_entry(ctx, entry: int):
-#     level, short_desc, img_url, long_desc = lore.get_lore(entry)
-#     footer_icon_url = 'https://i.imgur.com/VpMy8Yr.gif'
-#     databank_embed = discord.Embed(
-#         type='rich',
-#         title=short_desc,
-#         description=long_desc,
-#         color=0x00ffd9,
-#         url=img_url
-#         )
-#     databank_embed.set_image(url=img_url)
-#     databank_embed.set_footer(text=f"From the Level: {level}", icon_url=footer_icon_url)
-#     await ctx.send(embed=databank_embed)
-
-
-# Databank List
 @slash.subcommand(base="databank",
                   name="list",
                   description="Print out a numbered list of all entries in the databank."
